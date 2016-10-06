168ae88a4fc7bc6716862d4523ae4790afa5a907
dssachan <dssachan@amazon.com>
2016-10-06 11:11:22 +0530

    first commit

---
+++ src/main/java/main/basic/StockService.java
@@ -0,0 +1,8 @@
+package main.basic;
+
+/**
+ * Created by dssachan on 04/10/16.
+ */
+public interface StockService {
+    public double getPrice(Stock stock);
+}
---
+++ src/main/java/main/junitInteg/CalculatorService.java
@@ -0,0 +1,11 @@
+package main.junitInteg;
+
+/**
+ * Created by dssachan on 04/10/16.
+ */
+public interface CalculatorService {
+    public double add(double input1, double input2);
+    public double subtract(double input1, double input2);
+    public double multiply(double input1, double input2);
+    public double divide(double input1, double input2);
+}
